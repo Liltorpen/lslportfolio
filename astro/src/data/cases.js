83c81095f9ae76f58973b1c6e319e64d87f26357
Liltorpen <lsliltorp@gmail.com>
2026-02-28 13:37:01 +0100

    nyeste

---
+++ astro/src/data/cases.js
@@ -0,0 +1,44 @@
+export const casesData = [
+    {
+        title: "VISUALS",
+        tags: "[VIDEO] [SHORTFORM]",
+        video: "/videos/lsl_optimized.mp4",
+        description: `Denne video skal ses som et manifest for den kreative og eksperimenterende retning jeg kan tilbyde indenfor videoproduktion.<br><br>Med særligt henblik på musik.<br><br><i>Medie-aktiverne er blevet transformeret gennem omfattende redigering i Premiere Pro og After Effects for at skabe et nyt kunstnerisk udtryk. Eventuelt anvendt tredjeparts-materiale er benyttet i forbindelse med undervisning og er ikke tiltænkt kommerciel udnyttelse. Projektet er udført med respekt for immaterielle rettigheder.</i>`
+    },
+    {
+        title: "MERCH",
+        tags: "[MERCH] [GRAFIK] [MOCKUP]",
+        image: "/images/merch.webp",
+        description: `Designs som adskiller sig fra mængden - designs som <strong>skærer igennem</strong> støjen.<br><br>Jeg hjælper med at designe merch, hvad end det er til labels, brands eller kunstnere.`
+    },
+    {
+        title: "ALBUM COVER",
+        tags: "[PHOTO] [BILLEDREDIGERING]",
+        image: "/images/albumcover.webp",
+        description: `Her ses et eksempel på visuel identitet med hensyn til musikudgivelser.<br><br>Jeg tog udgangspunkt i nuværende trends, med fokus på at skabe noget som fanger øjet.<br><br>Udover at være visuelt fængende var det også vigtigt at have i mente, at visuelle identiteter såsom netop album covers fungerer som et anker på streamingtjenester.`
+    },
+    {
+        title: "KONCERTPLAKAT",
+        tags: "[TRYKTE MEDIER]",
+        image: "/images/koncertplakat.webp",
+        description: `Lamin optræder i Bella Arena d. 21 februar. 2026. Jeg udarbejdede derfor et eksempel på hvordan en koncertplakat, kunne se ud.<br><br>Lamins grafik er generelt inspireret fra den amerikanske scene og det tog jeg derfor også udgangspunkt i.`
+    },
+    {
+        title: "PRODUKTBILLEDER",
+        tags: "[PHOTO]",
+        image: "/images/produktbillede.webp",
+        description: `Vinyl overtog CD-markedet for første gang i 2022 siden 1987. Udover at være et funktionelt lydformat er det også blevet en form for merch.<br><br>På billedet ses Travis Scott's album "Utopia". Et eksempel på hvordan et produktbillede kunne se ud, det hjælper jeg naturligvis også med.`
+    },
+    {
+        title: "LOGO DESIGN",
+        tags: "[VEKTOR] [BRANDING]",
+        image: "/images/rebellion_logo.webp",
+        description: `Her ses et fiktivt eksempel på visuel identitet til et pladeselskab "Rebellion Records" som er et mindre pladeselskab i USA.<br><br>Deres identitet bygger på at være underdogs og det var derfor vigtigt at skabe noget visuelt som er imperfekt, men samtidig ser professionelt ud.`
+    },
+    {
+        title: "DYNAMISK CONTENT",
+        tags: "[PEOPLES CHOICE AWARD] [ANIMATION]",
+        video: "/videos/wanderer_optimized.mp4",
+        description: `Opgaven var at give liv til et maleri. Maleriet "Wanderer Above the Sea" udstråler bevægelse, også i statisk tilstand - det var en perfekt kandidat.<br><br>Ideen var at få seeren til at føle at de træder ind i maleriet.`
+    }
+];
