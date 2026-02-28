0d01fa2632d8e6245965d033bdedf647605a2e98
Liltorpen <lsliltorp@gmail.com>
2026-02-28 15:35:14 +0100

    nyeste

--- astro/src/data/cases.js
+++ astro/src/data/cases.js
@@ -3,7 +3,7 @@ export const casesData = [
         title: "VISUALS",
         tags: "[VIDEO] [SHORTFORM]",
         video: "/videos/lsl_optimized.mp4",
-        description: `Denne video skal ses som et manifest for den kreative og eksperimenterende retning jeg kan tilbyde indenfor videoproduktion.<br><br>Med særligt henblik på musik.<br><br><i>Medie-aktiverne er blevet transformeret gennem omfattende redigering i Premiere Pro og After Effects for at skabe et nyt kunstnerisk udtryk. Eventuelt anvendt tredjeparts-materiale er benyttet i forbindelse med undervisning og er ikke tiltænkt kommerciel udnyttelse. Projektet er udført med respekt for immaterielle rettigheder.</i>`
+        description: `Denne video skal ses som et manifest for den <strong>kreative og eksperimenterende</strong> retning jeg kan tilbyde indenfor videoproduktion. Med særligt henblik på musik.<br><br><i>Medie-aktiverne er blevet transformeret gennem omfattende redigering i Premiere Pro og After Effects for at skabe et nyt kunstnerisk udtryk.<br><br>Eventuelt anvendt tredjeparts-materiale er benyttet i forbindelse med undervisning og er ikke tiltænkt kommerciel udnyttelse. Projektet er udført med respekt for immaterielle rettigheder.</i>`
     },
     {
         title: "MERCH",
@@ -15,30 +15,30 @@ export const casesData = [
         title: "ALBUM COVER",
         tags: "[PHOTO] [BILLEDREDIGERING]",
         image: "/images/albumcover.webp",
-        description: `Her ses et eksempel på visuel identitet med hensyn til musikudgivelser.<br><br>Jeg tog udgangspunkt i nuværende trends, med fokus på at skabe noget som fanger øjet.<br><br>Udover at være visuelt fængende var det også vigtigt at have i mente, at visuelle identiteter såsom netop album covers fungerer som et anker på streamingtjenester.`
+        description: `Her ses et eksempel på <strong>visuel identitet</strong> med hensyn til musikudgivelser.<br><br>Jeg tog udgangspunkt i nuværende trends, med fokus på at skabe noget som <strong>fanger øjet</strong>.<br><br>Udover at være visuelt fængende var det også vigtigt at have i mente, at visuelle identiteter såsom netop album covers fungerer som et <strong>anker</strong> på streamingtjenester.`
     },
     {
         title: "KONCERTPLAKAT",
         tags: "[TRYKTE MEDIER]",
         image: "/images/koncertplakat.webp",
-        description: `Lamin optræder i Bella Arena d. 21 februar. 2026. Jeg udarbejdede derfor et eksempel på hvordan en koncertplakat, kunne se ud.<br><br>Lamins grafik er generelt inspireret fra den amerikanske scene og det tog jeg derfor også udgangspunkt i.`
+        description: `Lamin optræder i Bella Arena d. 21 februar. 2026. Jeg udarbejdede derfor et <strong>eksempel på</strong> hvordan <strong>en koncertplakat</strong>, kunne se ud.<br><br>Lamins grafik er generelt inspireret fra den amerikanske scene og det tog jeg derfor også udgangspunkt i.`
     },
     {
         title: "PRODUKTBILLEDER",
         tags: "[PHOTO]",
         image: "/images/produktbillede.webp",
-        description: `Vinyl overtog CD-markedet for første gang i 2022 siden 1987. Udover at være et funktionelt lydformat er det også blevet en form for merch.<br><br>På billedet ses Travis Scott's album "Utopia". Et eksempel på hvordan et produktbillede kunne se ud, det hjælper jeg naturligvis også med.`
+        description: `Vinyl overtog CD-markedet for første gang i 2022 siden 1987. Udover at være et funktionelt lydformat er det også blevet en form for merch.<br><br>På billedet ses Travis Scott's album "Utopia". Et <strong>eksempel på</strong> hvordan <strong>et produktbillede</strong> kunne se ud, det hjælper jeg naturligvis også med.`
     },
     {
         title: "LOGO DESIGN",
         tags: "[VEKTOR] [BRANDING]",
         image: "/images/rebellion_logo.webp",
-        description: `Her ses et fiktivt eksempel på visuel identitet til et pladeselskab "Rebellion Records" som er et mindre pladeselskab i USA.<br><br>Deres identitet bygger på at være underdogs og det var derfor vigtigt at skabe noget visuelt som er imperfekt, men samtidig ser professionelt ud.`
+        description: `Her ses et fiktivt eksempel på <strong>visuel identitet</strong> til et pladeselskab "Rebellion Records" som er et mindre pladeselskab i USA.<br><br>Deres identitet bygger på at være underdogs og det var derfor vigtigt at skabe noget visuelt som er <strong>imperfekt</strong>, men samtidig ser <strong>professionelt</strong> ud.`
     },
     {
         title: "DYNAMISK CONTENT",
         tags: "[PEOPLES CHOICE AWARD] [ANIMATION]",
         video: "/videos/wanderer_optimized.mp4",
-        description: `Opgaven var at give liv til et maleri. Maleriet "Wanderer Above the Sea" udstråler bevægelse, også i statisk tilstand - det var en perfekt kandidat.<br><br>Ideen var at få seeren til at føle at de træder ind i maleriet.`
+        description: `Opgaven var at give liv til et maleri. Maleriet <strong>"Wanderer Above the Sea"</strong> udstråler bevægelse, også i statisk tilstand - det var en perfekt kandidat.<br><br>Ideen var at få seeren til at føle at de træder ind i maleriet.`
     }
 ];
